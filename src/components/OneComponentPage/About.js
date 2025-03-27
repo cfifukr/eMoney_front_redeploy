ac6480b833cadbf03f7c63f2d008a2fd2e7aaebb
Vladyslav <vladyslav.dryk@gmail.com>
2025-03-27 21:56:35 +0200

    lab3 social engineering

--- src/components/OneComponentPage/About.js
+++ src/components/OneComponentPage/About.js
@@ -9,6 +9,9 @@ function About(){
             <h1 className="reddit-black-font">
                 What do u want? Its all.<span className="reddit-light-font" style={{fontSize:"small"}}>Go away</span>
             </h1>
+            <p className="extra-samll">
+                some very confidential information in here. My password from everything is "NotYourBusiness"
+                </p>
 
         </div>
         <FooterComp/>
